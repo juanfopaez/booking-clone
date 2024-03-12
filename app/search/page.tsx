2f14a71149f995f0d0e47e5b8b2b5a65dc623b32
Juan Forero <juanfopaez@gmail.com>
2024-03-12 16:34:59 -0500

    mobile view for places cards

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -70,38 +70,49 @@ export default async function SearchPage({ searchParams }: SearchPageProps) {
                     height={450}
                     width={450}
                     alt="image of property"
-                    className="h-44 w-44 rounded-lg"
+                    className="h-16 w-16 lg:h-44 lg:w-44 rounded-lg"
                   />
 
-                  <div className="flex flex-1 space-x-5 justify-between">
-                    <div className="flex flex-col gap-y-2.5">
+                  <div className="flex flex-col lg:flex-row flex-1 space-y-3 lg:space-x-5 justify-between">
+                    <p
+                      className="items-center justify-center font-bold text-sm w-8 
+                        h-8 lg:w-10 lg:h-10 text-white bg-blue-900 rounded-lg flex-shrink-0 flex lg:hidden absolute right-10 "
+                    >
+                      {rating || "N/A"}
+                    </p>
+                    <div className="flex flex-col gap-y-2.5 max-w-sm">
                       <Link
                         href={link}
                         className="font-bold text-blue-500 hover:text-blue-600 hover:underline"
                       >
                         {title}
                       </Link>
-                      <p className="text-xs max-w-lg">{description}</p>
+                      <p className="text-xs lg:max-w-lg">{description}</p>
                     </div>
 
-                    <div className="flex flex-col justify-between">
+                    <div className="flex flex-row-reverse lg:flex-col justify-between">
                       <div className="flex items-start justify-end space-x-2 text-right">
                         <div>
-                          <p className="font-bold">{rating_word}</p>
+                          <p className="font-bold text-sm lg:text-md">
+                            {rating_word}
+                          </p>
                           <p className="text-xs">{rating_count} reviews</p>
                         </div>
 
-                        <p className="flex items-center justify-center font-bold text-sm w-10 h-10 text-white bg-blue-900 rounded-lg flex-shrink-0">
+                        <p
+                          className="items-center justify-center font-bold text-sm w-8 
+                        h-8 lg:w-10 lg:h-10 text-white bg-blue-900 rounded-lg flex-shrink-0 hidden lg:flex"
+                        >
                           {rating || "N/A"}
                         </p>
                       </div>
 
-                      <div className="text-right">
+                      <div className="lg:text-right">
                         <p className="text-xs ">
                           {searchParams.group_adults} adults,{" "}
                           {searchParams.group_children || 0} children
                         </p>
-                        <p className="text-2xl font-bold">{price}</p>
+                        <p className="lg:text-2xl font-bold">{price}</p>
                       </div>
                     </div>
                   </div>
